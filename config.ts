8d3b773885ca768ae97f42ba17d3aa725b8d3ad9
pixeljellyfish <g3npixel@gmail.com>
2021-07-31 23:36:48 +1000

    suprabase(attempt?)
    - Add suprabase files/code
    - Test suprabase (new firebase??)
    - merge (maybe?)

---
+++ config.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDhL7zx7NLqBVMyUQrSnsTrm6qKcGF5ltI",
+    authDomain: "repo-pixeljellyfish-codes.firebaseapp.com",
+    databaseURL: "https://repo-pixeljellyfish-codes.firebaseio.com",
+    projectId: "repo-pixeljellyfish-codes",
+    storageBucket: "repo-pixeljellyfish-codes.appspot.com",
+    messagingSenderId: "278747946707",
+    appId: "1:278747946707:web:d07831897802ced13a3e26",
+    measurementId: "G-G8JRN19DC3"
+  };
