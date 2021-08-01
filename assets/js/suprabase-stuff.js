66a711bc5ef4347dd94c44467050f7f1f2437ff0
pixeljellyfish <g3npixel@gmail.com>
2021-08-01 22:41:03 +1000

    removed firebase stuff

---
+++ assets/js/suprabase-stuff.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database 
+const supabase = createClient("https://xyzcompany.supabase.co", "public-anon-key")
+
+
+
+
+https://bxtoqxpckzrfoqgyfqjs.supabase.co/rest/v1/?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzU3ODU5MCwiZXhwIjoxOTQzMTU0NTkwfQ.3SuYfj0jTpzHoy2gW2_4NyjkKce92SpSPqwl97hueqM
